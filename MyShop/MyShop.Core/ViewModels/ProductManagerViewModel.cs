7bd75770581f180a7693812f53d17079bb0f706d
Mark Moore <mark.c.moore@hotmail.com>
2019-12-16 12:32:09 -0600

    creates dropdown for categories

---
+++ MyShop/MyShop.Core/ViewModels/ProductManagerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyShop.Core.models;
+
+namespace MyShop.Core.ViewModels
+{
+    public class ProductManagerViewModel
+    {
+        public Product product { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
