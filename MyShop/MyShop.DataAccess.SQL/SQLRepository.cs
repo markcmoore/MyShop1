381bec39f54217905116d8843bf3398081a73bfd
Mark Moore <mark.c.moore@hotmail.com>
2019-12-17 10:36:54 -0600

    implements SQL DB

---
+++ MyShop/MyShop.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyShop.Core.Contracts;
+using MyShop.Core.models;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        internal DataContext context;
+        internal DbSet<T> dbSet;
+
+        public SQLRepository(DataContext context)
+        {
+            this.context = context;
+            dbSet = context.Set<T>();
+        }
+
+        public IQueryable<T> Collection()
+        {
+            return dbSet;
+        }
+
+        public void Commit()
+        {
+            context.SaveChanges();
+        }
+
+        public void Delete(string Id)
+        {
+            var t = Find(Id);//use the Find() below to get the correct object
+
+            //see if the found object is in a detached state.. (whatever that is.)
+            if (context.Entry(t).State == EntityState.Detached)
+            {//if so, attach it.....
+                dbSet.Attach(t);
+            }
+
+            dbSet.Remove(t);
+        }
+
+        public T Find(string Id)
+        {
+            return dbSet.Find(Id);
+        }
+
+        public void Insert(T t)
+        {
+            dbSet.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            dbSet.Attach(t);//you have to 'attach' the object... which is not the Entry
+            //tell EF the entry is modified and it will automatically update the correct object in the DB
+            context.Entry(t).State = EntityState.Modified;
+
+        }
+    }
+}
--- MyShop/MyShop.WebUI/App_Start/UnityConfig.cs
+++ MyShop/MyShop.WebUI/App_Start/UnityConfig.cs
@@ -2,6 +2,7 @@ using System;
 using MyShop.Core.Contracts;
 using MyShop.Core.models;
 using MyShop.DataAccess.InMemory;
+using MyShop.DataAccess.SQL;
 using Unity;
 
 namespace MyShop.WebUI
@@ -45,8 +46,8 @@ namespace MyShop.WebUI
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
 
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
 
         }
     }
