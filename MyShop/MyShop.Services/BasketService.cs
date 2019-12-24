8a64266820c2d548e7c86de621b2df2c257446ea
Mark Moore <mark.c.moore@hotmail.com>
2019-12-24 14:02:05 -0600

    Added basket to Db

---
+++ MyShop/MyShop.Services/BasketService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using MyShop.Core.Contracts;
+using MyShop.Core.models;
+
+namespace MyShop.Services
+{
+    public class BasketService
+    {
+        //these are recreated each time this BasketService is called so it's always up to date.
+        IRepository<Product> productContext;
+        IRepository<Basket> basketContext;
+
+        public const string BasketSessionName = "eCommerceBasket";
+
+        //use dependency injection to insert the contexts into this class object.
+        public BasketService(IRepository<Product> ProductContext, IRepository<Basket> BasketContext)
+        {
+            this.basketContext = BasketContext;
+            this.productContext = ProductContext;
+        }
+
+        private Basket GetBasket(HttpContextBase httpContext, bool createIfNull)
+        {
+            HttpCookie cookie = httpContext.Request.Cookies.Get(BasketSessionName);
+
+
+            Basket basket = new Basket();
+
+            if (cookie != null)
+            {
+                string basketId = cookie.Value;
+
+                if (!string.IsNullOrEmpty(basketId))
+                {
+                    basket = basketContext.Find(basketId);
+                }
+                else
+                {
+                    if (createIfNull)
+                    {
+                        basket = CreatNewBasket(httpContext);
+                    }
+                }
+            }
+            else
+            {
+                if (createIfNull)
+                {
+                    basket = CreatNewBasket(httpContext);
+                }
+            }
+            return basket;
+        }
+    }
+}
