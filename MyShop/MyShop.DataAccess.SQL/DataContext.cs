2f788d64e2c0243c04e14b8a47714b7ddaf76c3c
Mark Moore <mark.c.moore@hotmail.com>
2019-12-25 23:43:05 -0600

    Adds User Registration

--- MyShop/MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -10,15 +10,12 @@ namespace MyShop.DataAccess.SQL
 {
     public class DataContext : DbContext
     {
-        public DataContext() : base("DefaultConnection")
-        {
+        public DataContext() : base("DefaultConnection") {  }
 
-        }
-
-        public DbSet<Product>         Products            { get; set; }
-        public DbSet<ProductCategory> ProductCategories   { get; set; }
-        public DbSet<Basket>          Baskets             { get; set; }
-        public DbSet<BasketItem>      BasketItems         { get; set; }
-
+        public DbSet<Product>         Products          { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Basket>          Baskets           { get; set; }
+        public DbSet<BasketItem>      BasketItems       { get; set; }
+        public DbSet<Customer>        Customers         { get; set; }
     }
 }
